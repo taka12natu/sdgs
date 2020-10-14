2d49c4da924531c7e857178bbfeb448e96dcd3a7
vagrant <vagrant@localhost.localdomain>
2020-10-14 13:11:39 +0000

    set_root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root 'public/tops#top'
+
   devise_for :users, controllers: {
         registrations: 'public/registrations',
           sessions: 'public/sessions',
